67bc7c9d11e87f7ceb833f736c712bc50c94a346
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-07-17 11:35:36 -0700

    added function that handles both time and date and some comments

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,22 @@ var counter = 1
 // A variable with an empty array to store past searches in local storage
 var searchList = []
 
-//function to fetch forecast data based on user input
+// Setting time and date function 
+function timeDate(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    var year = date.getFullYear()
+    
+    var dayOutput =
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day + '/' + 
+        year;
+    return dayOutput;
+}
+
+// Function to fetch forecast data based on user input
 function callForecast(search) {
     var key = '5e6f355bc87b7f368b38aecdf2f70995';
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + search + "&appid=" + key;
@@ -30,6 +45,7 @@ function callForecast(search) {
         })
   };
 
+// Function to fetch weather data based on user input
 function callWeather(search) {
     var key = 'd49b1f6a58f01678d1f0b70e468590c6';
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=" + key;
@@ -43,5 +59,7 @@ function callWeather(search) {
         }
     })
 };
+
+
 
 
